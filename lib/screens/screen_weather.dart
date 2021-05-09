6d1518f090f4cbcae67bc2ec9761ee4f1b495a9d
dits1019 <jdits1019@gmail.com>
2021-05-09 20:50:48 +0900

    레이아웃 수정

--- lib/screens/screen_weather.dart
+++ lib/screens/screen_weather.dart
@@ -14,8 +14,8 @@ class _WeatherScreenState extends State<WeatherScreen> {
   var cityName;
   int temp;
   var wind;
-  var weather;
-  var detail_weather;
+  // var weather;
+  // dynamic detail_weather;
 
   @override
   void initState() {
@@ -27,8 +27,8 @@ class _WeatherScreenState extends State<WeatherScreen> {
     cityName = weatherData['name'];
     var temp2 = weatherData['main']['temp'];
     wind = weatherData['wind']['speed'];
-    weather = weatherData['weather']['main'];
-    detail_weather = weatherData['weather']['description'];
+    // weather = weatherData['weather']['main'];
+    // detail_weather = weatherData['weather']['description'];
 
     // round = 소수점 반올림
     temp = temp2.round();
@@ -39,27 +39,66 @@ class _WeatherScreenState extends State<WeatherScreen> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.blue[50],
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(cityName,
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-            Row(
-              children: [
-                Container(
-                    child: FaIcon(
-                  FontAwesomeIcons.temperatureHigh,
-                  size: 100,
-                )),
-                Text(
-                  '${temp.toString()}°C',
-                  style: TextStyle(fontSize: 30),
+        body: Center(
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 100, bottom: 60),
+                  child: Text(cityName,
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
                 ),
-              ],
-            )
-          ],
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 300),
+                  child: weather_view(FontAwesomeIcons.temperatureHigh,
+                      '${temp.toString()}°C', 10),
+                ),
+              ),
+              Align(
+                  alignment: Alignment.center,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 100),
+                    child: weather_view(
+                        FontAwesomeIcons.wind, wind.toString(), 20),
+                  )),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  width: double.infinity,
+                  child: Image.network(
+                      'https://cdn.pixabay.com/photo/2018/12/10/16/22/city-3867295_1280.png'),
+                ),
+              )
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget weather_view(icon, String text, double margin) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          child: FaIcon(
+            icon,
+            size: 50,
+          ),
+        ),
+        SizedBox(
+          width: margin,
+        ),
+        Text(
+          text,
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+      ],
     );
   }
 }
